7525a2aacc13a0b4ac6144d628e38537c3935972
Matt Pestritto <matt@pestritto.com>
2012-02-25 13:17:03 -0500

    creation of rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartDj::Application.config.secret_token = '50afdbe17c76c5078f5d81c2aaa1a8effd3e6a9c3224a72994e2c2e9dd616175f9138181c87ce1688125f7b5e892c5c224b7b281005cfb7919c1665b954ddb6c'
